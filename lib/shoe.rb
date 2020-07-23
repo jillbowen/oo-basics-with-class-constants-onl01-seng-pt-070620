41e7f5fb3b08ac87696679e8fcafb26c6755fdf6
Jill Short <jillian.b.short@gmail.com>
2020-07-23 01:26:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,9 +7,8 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    BRANDS << brand.uniq!
   end
-binding.pry
+
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
